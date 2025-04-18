6016be3eb1d881233a9cf2d4d40712590be07279
Manoela Urias <manoela.urias@treetech.com.br>
2025-04-18 11:43:11 -0300

    finaliza classe

--- TaskHome-API/Models/Tarefa.cs
+++ TaskHome-API/Models/Tarefa.cs
@@ -2,11 +2,31 @@
 {
     public class Tarefa
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid Id { get; set; } 
         public string Nome { get; set; }
         public string? Descricao { get; set; }
-        public DateTime? DataAgendada { get; set; }
-        public bool StatusDeConclusao { get; set; }
-        public DateTime DataCriada { get; set; } = DateTime.Now;
+        public DiaAgendamento Dia { get; set; }
+        public PrioridadeTarefa Propriedade { get; set; }
+        public bool RepetirSemanalmente { get; set; }
+        public bool Concluida { get; set; }
+        public DateTime DataCriada { get; set; }
+    }
+
+    public enum PrioridadeTarefa
+    {
+        Baixa,
+        Media,
+        Alta
+    }
+
+    public enum DiaAgendamento
+    {
+        Segunda,
+        Terca,
+        Quarta,
+        Quinta,
+        Sexta,
+        Sabado,
+        Domingo
     }
 }
